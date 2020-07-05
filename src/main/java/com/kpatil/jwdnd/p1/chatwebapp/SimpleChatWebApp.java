aed37b967a36066471e5e961e4f8b90cff84e358
knpatil <patilkam@yahoo.com>
2020-07-05 00:19:43 -0700

    simple chat web app in spring boot

---
+++ src/main/java/com/kpatil/jwdnd/p1/chatwebapp/SimpleChatWebApp.java
@@ -0,0 +1,13 @@
+package com.kpatil.jwdnd.p1.chatwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleChatWebApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpatil/jwdnd/p1/chatwebapp/SimpleChatWebAppTests.java
@@ -0,0 +1,13 @@
+package com.kpatil.jwdnd.p1.chatwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatWebAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
